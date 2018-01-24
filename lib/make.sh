c53ce168675cc336378cf62ddf6748f6c861ad52
clement <clement10601@gmail.com>
2018-01-24 16:32:13 +0800

    fix CPATH

--- lib/make.sh
+++ lib/make.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-CUDA_PATH=/usr/local/cuda/
+CPATH=/opt/cuda/include
+CUDA_PATH=/opt/cuda/
 
 python setup.py build_ext --inplace
 rm -rf build
 
-CUDA_ARCH="-gencode arch=compute_30,code=sm_30 \
-           -gencode arch=compute_35,code=sm_35 \
-           -gencode arch=compute_50,code=sm_50 \
+CUDA_ARCH="-gencode arch=compute_50,code=sm_50 \
            -gencode arch=compute_52,code=sm_52 \
            -gencode arch=compute_60,code=sm_60 \
            -gencode arch=compute_61,code=sm_61 \
